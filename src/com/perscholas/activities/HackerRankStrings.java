1c1e9102a77c10e8279fd25f5504915a83eede91
Edward Barcenas <edward.barcenas16@gmail.com>
2022-11-28 15:29:07 -0600

    updates to hackerRank Strings

--- src/com/perscholas/activities/HackerRankStrings.java
+++ src/com/perscholas/activities/HackerRankStrings.java
@@ -68,5 +68,100 @@ public class HackerRankStrings {
        
        sc.close();
 	}
+	
+	public static void substring(Scanner in) {
+		/*
+		 * Given a string, , and two indices,  and , print a substring consisting of all characters in the inclusive range from  to . You'll find the String class' substring method helpful in completing this challenge.
+
+		Input Format
+		
+		The first line contains a single string denoting .
+		The second line contains two space-separated integers denoting the respective values of  and .
+		
+		Constraints
+		
+		String  consists of English alphabetic letters (i.e., ) only.
+		Output Format
+		
+		Print the substring in the inclusive range from  to .
+		
+		Sample Input
+		
+		Helloworld
+		3 7
+		Sample Output
+		
+		lowo
+		 */
+		
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+        String output = S.substring(start, end);
+        System.out.println(output);
+	}
+	
+	public static void substringComparisons(String s, int k) {
+		/*
+		 * We define the following terms:
+
+			Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+			For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+			
+			A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+			Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+			
+			Function Description
+			
+			Complete the getSmallestAndLargest function in the editor below.
+			
+			getSmallestAndLargest has the following parameters:
+			
+			string s: a string
+			int k: the length of the substrings to find
+			Returns
+			
+			string: the string ' + "\n" + ' where and are the two substrings
+			Input Format
+			
+			The first line contains a string denoting .
+			The second line contains an integer denoting .
+			
+			Constraints
+			
+			 consists of English alphabetic letters only (i.e., [a-zA-Z]).
+			Sample Input 0
+			
+			welcometojava
+			3
+			Sample Output 0
+			
+			ava
+			wel
+		 */
+		
+        String smallest = "";
+        String largest = "";
+        String[] list;
+        
+        // Complete the function
+        for (int i = 0; i < s.length(); i++) {
+        	list.add(s.substring(i, k));
+        }
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+		
+	}
+	
+	public static void stringReverse() {
+		
+	}
+	
+	public static void stringTokens() {
+		
+	}
 
 }
